fa03dc1678dd8a44e1ebb57e340dc4bd1270b854
vsh2607 <evolutionvalen@gmail.com>
2024-09-02 08:20:43 +0700

    feat : adding edit and info page for master farmer

--- app/Http/Controllers/MasterFarmerController.php
+++ app/Http/Controllers/MasterFarmerController.php
@@ -26,8 +26,6 @@ class MasterFarmerController extends Controller
                 $deleteButton = "&nbsp;<a href='#' class='btn  btn-danger d-inline-block'><i class='fas fa-trash'></i></a>";
                 $mapButton = '<button class="btn btn-success d-inline-block btn-map" type="button" data-location="' . $model->land_location . '"><i class="fas fa-map-marked-alt"></i></button>';
                 return  $mapButton . $infoButton . $editButton;
-
-
             })
             ->editColumn('land_area', function ($model) {
                 return $model->land_area . ' m<sup>2</sup>';
@@ -83,5 +81,63 @@ class MasterFarmerController extends Controller
             DB::rollBack();
             return redirect('master-data/master-farmer')->with('error', $e->getMessage());
         }
+    }
+
+    public function editForm($id)
+    {
+        $data = MasterFarmer::find($id);
+        return view('master-farmer.edit', ['data' => $data]);
+    }
+
+    public function updateData(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'land_type' => 'required',
+            'handphone_number' => 'required',
+            'land_area' => 'required',
+            'land_location' => 'required',
+            'fertilizer_quantity_owned' => 'required',
+            'fertilizer_quantity_needed' => 'required',
+        ]);
+
+        DB::beginTransaction();
+        try {
+            $data = MasterFarmer::find($id);
+            if (!$data) {
+                MasterFarmer::updateOrCreate([
+                    'id' => $id,
+                    'name' => $request->name,
+                    'land_type' => $request->land_type,
+                    'handphone_number' => $request->handphone_number,
+                    'land_area' => $request->land_area,
+                    'land_location' => $request->land_location,
+                    'fertilizer_quantity_owned' => $request->fertilizer_quantity_owned,
+                    'fertilizer_quantity_needed' => $request->fertilizer_quantity_needed,
+                ]);
+            } else {
+                $data->update([
+                    'name' => $request->name,
+                    'land_type' => $request->land_type,
+                    'handphone_number' => $request->handphone_number,
+                    'land_area' => $request->land_area,
+                    'land_location' => $request->land_location,
+                    'fertilizer_quantity_owned' => $request->fertilizer_quantity_owned,
+                    'fertilizer_quantity_needed' => $request->fertilizer_quantity_needed,
+                ]);
+            }
+
+            DB::commit();
+            return redirect('master-data/master-farmer')->with('success', 'Data berhasil diperbarui!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect('master-data/master-farmer')->with('error', $e->getMessage());
+        }
+    }
+
+    public function viewForm($id){
+        $data = MasterFarmer::find($id);
+        return view('master-farmer.info', ['data' => $data]);
+
     }
 }
---
+++ resources/views/master-farmer/info.blade.php
@@ -0,0 +1,207 @@
+@extends('adminlte::page')
+
+@section('title', 'Tambah Data Petani')
+
+@section('adminlte_css_pre')
+    <link rel="icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('leafletjs/leaflet.css') }}">
+
+
+    <style>
+        .required::after {
+            content: '*';
+            color: red;
+        }
+    </style>
+@endsection
+
+@section('content_header')
+    <h1>Tambah Data Petani</h1>
+@stop
+
+@section('content')
+    @if (session('error'))
+        <div class="alert alert-danger mb-2">
+            {{ session('error') }}
+        </div>
+    @endif
+    @if (session('success'))
+        <div class="alert alert-success mb-2">
+            {{ session('success') }}
+        </div>
+    @endif
+    <div class="container-fluid" style="margin-top:20px; text-transform: uppercase;">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <label class="required" for="name">Nama Petani</label>
+                                <input type="text" required name="name" id="name" class="form-control my-input" placeholder="Nama Petani" value="{{ $data->name }}" readonly>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <label class="required" for="land_type">Tipe Lahan</label>
+                                <input type="text" name="land_type" id="land_type" class="form-control" value="{{ $data->land_type == "OWNED" ? "MILIK SENDIRI" : "GARAP ORANG LAIN" }}" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <label class="required" for="handphone_number">Nomor Handphone</label>
+                                <input type="text" required name="handphone_number" id="handphone_number" class="form-control my-input-int" placeholder="Nomor Handphone" value="{{ $data->handphone_number }}" readonly>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <label class="required" for="land_area">Luas Lahan (m<sup>2</sup>)</label>
+                                <input type="text" required name="land_area" id="land_area" class="form-control my-input-int" placeholder="Luas Lahan" value="{{ $data->land_area }}" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <label for="land_location" class="required">Lokasi Lahan</label>
+                                <div class="input-group">
+                                    <input required type="text" name="land_location" id="land_location" class="form-control my-input" placeholder="Lokasi Lahan" value="{{ $data->land_location }}" readonly>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-secondary btn-map" type="button">
+                                            <i class="fas fa-map-marked-alt"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <label class="required" for="fertilizer_quantity_owned">Jumlah Pupuk Dimiliki (KG)</label>
+                                <input required type="text" name="fertilizer_quantity_owned" id="fertilizer_quantity_owned" class="form-control my-input-decimal" placeholder="Jumlah Pupuk Dimiliki" value="{{ $data->fertilizer_quantity_owned }}" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <label class="required" for="fertilizer_quantity_needed">Jumlah Pupuk Dibutuhkan (KG)</label>
+                                <input required type="text" name="fertilizer_quantity_needed" id="fertilizer_quantity_needed" class="form-control my-input-decimal" placeholder="Jumlah Pupuk Dibutuhkan" value="{{ $data->fertilizer_quantity_needed }}" readonly>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div id="myModal" class="modal fade" role="dialog">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">PETA LOKASI</h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                            <div id="map" style="width: 100%; height:400px;"></div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+    </div>
+
+
+@stop
+
+@section('plugins.Datatables', true)
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="/leafletjs/leaflet.js"></script>
+    <script>
+        $(document).ready(function() {
+
+            function getPolygonCenter(coords) {
+                let latSum = 0;
+                let lngSum = 0;
+                let numCoords = coords.length;
+
+                for (let i = 0; i < numCoords; i++) {
+                    latSum += coords[i][0]; // Latitude
+                    lngSum += coords[i][1]; // Longitude
+                }
+
+                return [latSum / numCoords, lngSum / numCoords];
+            }
+
+
+            let polygonLayer = null;
+            let polygonCoords = [];
+            let coordString = '';
+
+            let location = $("#land_location").val();
+            coordString = location;
+            let coordArray = coordString.split(',');
+
+            for (let i = 0; i < coordArray.length; i += 2) {
+                polygonCoords.push([parseFloat(coordArray[i]), parseFloat(coordArray[i + 1])]);
+            }
+
+            let centerCoord = getPolygonCenter(polygonCoords);
+
+            let map = L.map('map').setView(centerCoord, 17);
+
+
+            let tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            polygonLayer = L.polygon(polygonCoords, {
+                color: 'red'
+            }).addTo(map);
+
+
+            $('.btn-map').click(function() {
+                $('#myModal').modal('show');
+                setTimeout(function() {
+                    map.invalidateSize()
+                }, 500);
+            });
+
+            $('.btn-save-map').click(function() {
+                $('#land_location').val('');
+                $('#land_location').val(polygonCoords);
+                $('#myModal').modal('hide');
+            });
+
+            $('.btn-reset-map').click(function() {
+                polygonCoords = [];
+                if (polygonLayer) {
+                    map.removeLayer(polygonLayer);
+                }
+
+                if (window.polygon) {
+                    map.removeLayer(window.polygon);
+                }
+            });
+
+
+            $(".my-input").on('input', function() {
+                $(this).val($(this).val().toUpperCase());
+            })
+
+            $(".my-input-int").on('input', function() {
+                this.value = this.value.replace(/[^0-9]/g, '');
+            });
+
+            $(".my-input-decimal").on('input', function() {
+                this.value = this.value.replace(/[^0-9.]/g, '');
+            });
+        });
+    </script>
+@stop
